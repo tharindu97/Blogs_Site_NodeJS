36f635dcab7a34e02c226d5a28f581db315f4572
tharindu97 <tharindukavishna@gmail.com>
2019-11-23 15:12:47 +0530

    2_3

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+const path = require('path')
 const express = require('express')
 
 const app = new express()
 
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+})
+
 
 app.listen(4000, () => {
-    console.log('App Listening on port')
+    console.log('App Listening on port 4000')
 })
