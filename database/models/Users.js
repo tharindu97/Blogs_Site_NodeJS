605e7122f8fc0178bfd611203ccd62fd56619352
tharindu97 <tharindukavishna@gmail.com>
2020-04-15 18:34:24 +0530

    30_compeleted

--- database/models/Users.js
+++ database/models/Users.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const UserSchema = new mongoose.Schema({
     username: String,
     email: String,
     password:String
 });
+
+UserSchema.pre('save', function(next){
+    const user = this;
+    bcrypt.hash(user.password, 10, function(error, encrupted){
+        user.password = encrupted;
+        next();
+    });
+});
+
 module.exports = mongoose.model('User', UserSchema);
