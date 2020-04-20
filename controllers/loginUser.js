2a2dd272cf9dababccf664903eb593d92163baa4
tharindu97 <tharindukavishna@gmail.com>
2020-04-20 20:03:48 +0530

    33_completed

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,5 +1,21 @@
 const User = require('../database/models/Users');
+const bcrypt = require('bcrypt');
 
 module.exports = (req,res) =>{
-    res.redirect('/');
+    const { email, password } = req.body;
+    //try to find the user.
+    User.findOne({ email }, (error, user) => {
+        if(user){
+            //compare user password.
+            bcrypt.compare(password, user.password, (error, same) =>{
+                if(same){
+                    //store user session
+                    res.redirect('/')
+                }else{
+                    res.redirect('/auth/login')
+                }
+            })
+        }
+    });
+
 }
