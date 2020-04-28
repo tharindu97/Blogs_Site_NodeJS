4a4b6bcc872d4db8f466d71e1d4b2f453a04e9f1
tharindu97 <tharindukavishna@gmail.com>
2020-04-28 22:03:14 +0530

    bug_fixed

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -2,7 +2,6 @@ const Post = require('../database/models/Post');
 
 module.exports = async (req, res) => {
     const posts = await Post.find({}).populate('author');
-    console.log(posts);
     res.render('index', {
         posts
     })
